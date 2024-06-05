df5522a8e1e11c597e5da97da2dc7904cc9d5830
Ehud Ettun <ehudettun@gmail.com>
2024-06-04 22:02:32 -0400

    disable caching

--- app/api/ec2-instances/route.ts
+++ app/api/ec2-instances/route.ts
@@ -4,7 +4,8 @@ import Instance from '../../../models/Instance';
 import { EC2Client, DescribeInstancesCommand } from '@aws-sdk/client-ec2';
 
 const REGIONS = [
-  'us-east-1', 'us-east-2'
+  'us-east-1', 'us-east-2', 'us-west-1', 'us-west-2',
+  // Add other regions as needed
 ];
 
 let lastUpdateTimestamp = 0;
@@ -22,9 +23,6 @@ interface InstanceType {
 
 const fetchAndStoreEC2Instances = async () => {
   await connectMongo();
-
-  console.log("AWS_ACCESS_KEY_ID:", process.env.AWS_ACCESS_KEY_ID);
-  console.log("AWS_SECRET_ACCESS_KEY:", process.env.AWS_SECRET_ACCESS_KEY);
 
   for (const region of REGIONS) {
     const ec2Client = new EC2Client({
@@ -65,16 +63,20 @@ const fetchAndStoreEC2Instances = async () => {
 export async function GET(req: NextRequest) {
   const currentTime = Date.now();
 
-  if (currentTime - lastUpdateTimestamp < 1000) {
+  if (currentTime - lastUpdateTimestamp < 30000) {
     // Less than 30 seconds since the last update
     console.log('Returning cached data');
     try {
       await connectMongo();
       const instances = await Instance.find().sort({ updatedAt: -1 }).limit(100);
-      return NextResponse.json(instances, { status: 200 });
+      const response = NextResponse.json(instances, { status: 200 });
+      response.headers.set('Cache-Control', 'no-store');
+      return response;
     } catch (error) {
       console.error('Error fetching instances from MongoDB:', error);
-      return NextResponse.json({ error: 'Failed to fetch EC2 instances' }, { status: 500 });
+      const response = NextResponse.json({ error: 'Failed to fetch EC2 instances' }, { status: 500 });
+      response.headers.set('Cache-Control', 'no-store');
+      return response;
     }
   }
 
@@ -83,9 +85,13 @@ export async function GET(req: NextRequest) {
   try {
     await fetchAndStoreEC2Instances();
     const instances = await Instance.find().sort({ updatedAt: -1 }).limit(100);
-    return NextResponse.json(instances, { status: 200 });
+    const response = NextResponse.json(instances, { status: 200 });
+    response.headers.set('Cache-Control', 'no-store');
+    return response;
   } catch (error) {
     console.error('Error fetching instances from MongoDB:', error);
-    return NextResponse.json({ error: 'Failed to fetch EC2 instances' }, { status: 500 });
+    const response = NextResponse.json({ error: 'Failed to fetch EC2 instances' }, { status: 500 });
+    response.headers.set('Cache-Control', 'no-store');
+    return response;
   }
 }
